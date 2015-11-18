b7cd97a7a3ccb6a6134702c49c172c00ff2abfbc
lizhang <ycy828f@gmail.com>
2015-11-17 16:26:50 -0800

    lz - final commit before concurrency erlang

--- lib_misc.erl
+++ lib_misc.erl
@@ -1,6 +1,7 @@
 -module(lib_misc).
 -export([for/3, qsort/1, pythag/1, perms/1, max/2, odds_and_evens1/1,
-          odds_and_evens2/1, odds_and_evens_no_case/1]).
+          odds_and_evens2/1, odds_and_evens_no_case/1, count_characters/1,
+          sqrt/1]).
 for(Max, Max, F) -> [F(Max)];
 for(I, Max, F) -> [F(I)|for(I+1, Max, F)].
 qsort([]) -> []; % Just beautiful.
@@ -44,3 +45,18 @@ odds_and_evens_acc1_help(1, H, T, Odds, Evens) ->
   odds_and_evens_acc1(T, [H|Odds], Evens);
 odds_and_evens_acc1_help(0, H, T, Odds, Evens) ->
   odds_and_evens_acc1(T, Odds, [H|Evens]).
+% count_characters
+count_characters(Str) -> count_characters(Str, #{}).
+count_characters([H|T], X) ->
+  case (maps:is_key(H, X)) of
+    true -> N = maps:get(H, X), count_characters(T, X#{H:=N+1});
+    false -> count_characters(T, X#{H=>1})
+  end;
+count_characters([], X) -> X.
+% count_characters([H|T], #{ H := N }=X) -> count_characters(T, X#{ H := N+1 });
+% count_characters([H|T], X) -> count_characters(T, X#{ H => 1 });
+% count_characters([], X) -> X.
+sqrt(X) when X < 0 ->
+  error({sqaureRootNegativeArgument, X});
+sqrt(X) ->
+  math:sqrt(X).
